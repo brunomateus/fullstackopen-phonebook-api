5ee37a15a3815137b07462fd4572da3484b59a4f
Bruno Mateus <brunomateus@gmail.com>
2025-05-15 16:09:15 -0300

    Step 5

--- index.js
+++ index.js
@@ -55,6 +55,33 @@ app.delete('/api/persons/:id', (request, response) => {
   response.status(204).end()
 })
 
+app.post('/api/persons', (request, response) => {
+  const body = request.body
+
+  if (!body.name) {
+    return response.status(400).json({
+      error: 'name missing',
+    })
+  }
+
+  if (!body.number) {
+    return response.status(400).json({
+      error: 'number missing',
+    })
+  }
+
+  const person = {
+    name: body.name,
+    number: body.number,
+    id: Math.floor(Math.random() * 10000 + persons.length + 1),
+  }
+
+  persons = persons.concat(person)
+
+  response.json(person)
+})
+
+
 app.get('/api/info', (request, response) => {
   const date = new Date()
   const info = `
