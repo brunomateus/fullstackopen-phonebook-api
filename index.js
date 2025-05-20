9d91b35f796fe26366905cb0c9843d81a64a7651
Bruno Mateus <brunomateus@gmail.com>
2025-05-20 11:16:26 -0300

    Step 17

--- index.js
+++ index.js
@@ -52,37 +52,80 @@ app.delete('/api/persons/:id', (request, response, next) => {
 })
 
 app.post('/api/persons', (request, response) => {
-  const body = request.body
+  const { name, number } = request.body
 
-  if (!body.name) {
+  if (!name) {
     return response.status(400).json({
       error: 'name missing',
     })
   }
 
-  if (persons.find((person) => person.name === body.name)) {
-    return response.status(400).json({
-      error: 'name must be unique',
-    })
-  }
-
-  if (!body.number) {
+  if (!number) {
     return response.status(400).json({
       error: 'number missing',
     })
   }
 
   const person = new Person({
-    name: body.name,
-    number: body.number,
+    name,
+    number,
   })
-  
-  person.save().then(result => {
-    console.log(`added ${person.name} number ${person.number} to phonebook`)
-    response.json(person)
+
+  Person.findOne({ name: person.name }).then(existingPersons => {
+    console.log('existingPersons', existingPersons)
+    if (existingPersons) {
+      const putForm = `
+        <form id="putForm" method="PUT" action="$/api/persons/${existingPersons._id}">
+          <input type="hidden" name="data" value="${JSON.stringify(request.body)}">
+        </form>
+        <script>
+          document.getElementById('putForm').submit();
+        </script>
+      `;
+
+      response.send(putForm);
+    } else {
+      person.save().then(result => {
+        console.log(`added ${person.name} number ${person.number} to phonebook`)
+        response.json(person)
+      })
+    }
   })
+
 })
 
+
+app.put('/api/persons/:id', (request, response, next) => {
+  const { name, number } = request.body
+
+  if (!name) {
+    return response.status(400).json({
+      error: 'name missing',
+    })
+  }
+
+  if (!number) {
+    return response.status(400).json({
+      error: 'number missing',
+    })
+  }
+
+  const person = {
+    name,
+    number,
+  }
+
+  Person.findOneAndUpdate({ _id: request.params.id }, person, { new: true })
+  .then(updatedPerson => {
+    if (updatedPerson) {
+      response.json(updatedPerson)
+    } else {
+      return response.status(404).json({
+        error: 'person not found',
+      })
+    }
+  }).catch(error => next(error))
+})
 
 app.get('/api/info', (request, response) => {
   const date = new Date()
