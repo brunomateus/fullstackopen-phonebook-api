9c78b64c04dd5afcf8c63976dcba0f5630310de8
Bruno Mateus <brunomateus@gmail.com>
2025-05-20 10:16:40 -0300

    Step 15 and 16

--- index.js
+++ index.js
@@ -18,29 +18,6 @@ morgan.token('body', (req) => {
 
 app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
-let persons = [
-    { 
-      id: 1,
-      name: "Arto Hellas", 
-      number: "040-123456"
-    },
-    { 
-      id: 2,
-      name: "Ada Lovelace", 
-      number: "39-44-5323523"
-    },
-    { 
-      id: 3,
-      name: "Dan Abramov", 
-      number: "12-43-234345"
-    },
-    { 
-      id: 4,
-      name: "Mary Poppendieck", 
-      number: "39-23-6423122"
-    }
-]
-
 app.use(express.json())
 
 app.get('/api/persons', (request, response) => {
@@ -53,29 +30,25 @@ app.get('/api/persons', (request, response) => {
 })
 
 
-app.get('/api/persons/:id', (request, response) => {
+app.get('/api/persons/:id', (request, response, next) => {
   Person.findById(request.params.id).then(person => {
-    console.log('person', person)
-    if(person == null) {
+    if(person) {
+      response.json(person)
+    } else {
       return response.status(404).json({
         error: 'person not found',
       })
     }
-    response.json(person)
   })
+  .catch(error => next(error))
 })
 
-app.delete('/api/persons/:id', (request, response) => {
-  const id = request.params.id
-  const numberOfPersons = persons.length
-  persons = persons.filter((note) => note.id != id)
-
-  if(numberOfPersons === persons.length) {
-    return response.status(404).json({
-      error: 'person not found',
-    })
-  }
-  response.status(204).end()
+app.delete('/api/persons/:id', (request, response, next) => {
+  Person.findByIdAndDelete(request.params.id)
+  .then(result => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
 })
 
 app.post('/api/persons', (request, response) => {
@@ -119,6 +92,18 @@ app.get('/api/info', (request, response) => {
   `
   response.send(info)
 })
+
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  } 
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 
 const PORT = process.env.PORT || 3001
