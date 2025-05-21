a34638dfbe38dcfce5ecfeb7d3a2937fa9152f8b
Bruno Mateus <brunomateus@gmail.com>
2025-05-20 21:07:34 -0300

    Step 20 and 21

--- models/person.js
+++ models/person.js
@@ -17,9 +17,18 @@ const personSchema = new mongoose.Schema({
   name: {
     type: String,
     required: true,
-    minLength: 3,
+    minLength: [3, 'Name must be at least 3 characters long'],
   },
-  number: String,
+  number: {
+    type: String,
+    required: true,
+    validate: {
+      validator: function(v) {
+        return /\d{2,3}-\d+/.test(v)
+      },
+      message: props => `${props.value} is not a valid phone number!`
+    },
+  },
 })
 
 personSchema.set('toJSON', {
